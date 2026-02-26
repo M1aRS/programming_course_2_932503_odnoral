a599bec2a32397bb25c55b038fb512f09e90ee1a
Bamich <neket.lut@gmail.com>
2026-02-26 13:39:15 +0700

    9 лаба, 1 упражнение сделано

--- labs/lab09_strings/Lab09/src/main.cpp
+++ labs/lab09_strings/Lab09/src/main.cpp
@@ -1,21 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 
 // Lab 09
-// TODO: реализуйте решение по заданию в labs/lab09_strings/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
+
 int main() {
-    // Упражнения 1–3: демонстрация/анализ строковых функций (без ввода).
-    // TODO: добавьте примеры и вывод результатов.
+    // Упражнеие 1.
+    int word_count = 0;
+    char f_first_word[200]; char f_second_word[200];
+    std::cin.getline(f_first_word, 200);
+    std::cout << "Length: " << strlen(f_first_word) << "\n";
+    std::cin.getline(f_second_word, 200);
+    if (strcmp(f_first_word, f_second_word) == 0) {std::cout << f_first_word << " == " << f_second_word << "\n";}
+    else { std::cout << f_first_word << " != " << f_second_word << "\n";}
+    std::cout << f_first_word << " + " << f_second_word << " = "; std::cout << strcat(f_first_word, f_second_word) << "\n";
+       
+    std::string f_first_string, f_second_string, f_temp_string;
+    std::getline(std::cin, f_first_string); 
+    int len1 = f_first_string.length(); int len2 = f_second_string.length();
+    std::cout << "Length: " << len1 << "\n";  std::getline(std::cin, f_second_string);
+    f_temp_string = f_first_string; f_temp_string.append(" with " + f_second_string);
+    std::cout << f_temp_string;
+    std::cout << "PIZDAAAA";
 
     // Упражнение 4: ввод количества слов и самих слов.
-    int word_count = 0;
-    if (!(std::cin >> word_count)) {
+    /*if (!(std::cin >> word_count)) {
         return 0;
     }
 
@@ -26,7 +37,7 @@ int main() {
         std::cin >> word;
         words.push_back(word);
     }
-
+    */
     // TODO: выведите слова с четными номерами (2, 4, 6, ...), по одному в строке.
     return 0;
 }
