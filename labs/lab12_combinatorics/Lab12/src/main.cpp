1e4b05da3a5250a769788568827f8f5d0582d753
M1aRS <odnoralgrigoriy@gmail.com>
2026-03-17 00:50:49 +0700

    Я ваще то делаю или нет?

--- labs/lab12_combinatorics/Lab12/src/main.cpp
+++ labs/lab12_combinatorics/Lab12/src/main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 
 // Lab 12
-void v_lob_i_tupo(char **matrix, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == '.') { 
-                matrix[i][j] = 'X';   
+int solution_count = 0;
 
-                for (int k = 1; i + k < n; k++) {
-                    matrix[i + k][j] = '~';
-                    if (j + k < n) matrix[i + k][j + k] = '~';
-                    if (j - k >= 0) matrix[i + k][j - k] = '~';
-                }
-
-                for (int l = j + 1; l < n; l++) {
-                    matrix[i][l] = '~';
-                }
-                break; 
+void v_lob_i_tupo(char **matrix, int n, int row) {
+    if (row == n){
+        solution_count++;
+        printf("Solutions: %d \n", solution_count);
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                printf("%c ", matrix[i][j]);
             }
+            printf("\n");
+        }
+    }
+
+    for (int col = 0; col < n; col++) {
+        bool is_safe_for_queen = true;
+
+        for (int i = 1; i <= row; i++) {
+            if (matrix[row - i][col] == 'Q') { is_safe_for_queen = false; break; }
+            if (col - i >= 0 && matrix[row - i][col - i] == 'Q'){ is_safe_for_queen = false; break; }
+            if (col + i < n && matrix[row - i][col + i] == 'Q'){ is_safe_for_queen = false; break; }
+        }
+
+        if (is_safe_for_queen) {
+            matrix[row][col] = 'Q';
+            v_lob_i_tupo(matrix, n, row + 1);
+            matrix[row][col] = '.';
         }
     }
 }
@@ -39,12 +49,10 @@ int main() {
         }
     }
 
-    v_lob_i_tupo(matrix, N);
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){    
-                printf("%2c ", matrix[i][j]);
-            }
-            printf("\n");
-    }
+    v_lob_i_tupo(matrix, N, 0);
+    printf("\nSolutions for N=%d: %d\n", N, solution_count);
+    for (int i = 0; i < N; i++) delete[] matrix[i];
+    delete[] matrix;
+    
     return 0;
 }
