a887cdc02d8ef3b72646c7795eea43508875733a
aleksey <aurst2007@yandex.ru>
2026-03-31 22:33:23 +0700

    Лаба 13. Добавил ввод матрицы коэффициентов, вывод матрицы

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 
 // Lab 13
-// TODO: реализуйте решение по заданию в labs/lab13_linear_algebra/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
-//asadasdasd
+
+void print_matrix(int **matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main() {
-    // TODO
+    FILE* input_file = fopen("input.txt", "r");
+    if (input_file == nullptr) {
+        std::cout << "Error opening file!" << std::endl;
+        return 1;
+    }
+    int M, N;
+    fscanf(input_file, "%d %d", &M, &N);
+    int **coeffitients = new int *[M];
+    for (int i = 0; i < M; i++) {
+        coeffitients[i] = new int[N];
+        for (int j = 0; j < N; j++) {
+            fscanf(input_file, "%d", &coeffitients[i][j]);
+        }
+    }
+    print_matrix(coeffitients, M, N);
+    fclose(input_file);
     return 0;
 }
