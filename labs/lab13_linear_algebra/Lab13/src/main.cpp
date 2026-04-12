5dccdb1c505149501de4a8fa67a3b04951f3900b
M1aRS <odnoralgrigoriy@gmail.com>
2026-04-12 22:14:28 +0700

    Refactor triangularization logic and update output formatting

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -4,58 +4,66 @@
 
 // Lab 13
 
-void print_matrix(double **matrix, int rows, int cols) {
+/*void print_matrix(double **matrix, int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             printf("%.3lf ", matrix[i][j]);
         }
         printf("\n");
     }
-}
+}*/
 
 void print_matrix_to_file(FILE* out, double **matrix, int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             fprintf(out, "%.3lf ", matrix[i][j]);
+            printf("%.3lf ", matrix[i][j]);
         }
         fprintf(out, "\n");
+        printf("\n");
     }
     fprintf(out, "--------------------------\n");
+    printf("----- \n");
 }
 
-void triangularize(double **matrix, int rows, int cols) {
-    for (int k = 0; k < rows; k++) {
-        int max_row = k;
-        for (int i = k + 1; i < rows; i++) {
-            if (std::abs(matrix[i][k]) > std::abs(matrix[max_row][k])) {
-                max_row = i;
+int triangularize(double **matrix, int rows, int cols) {
+    for (int i = 0; i < cols-1; i++) {
+        int v = i;
+        for (int j = i + 1; j < rows; j++) {
+            if (std::abs(matrix[j][i]) > std::abs(matrix[v][i])) {
+                v = j;
             }
         }
 
-        double *temp = matrix[k];
-        matrix[k] = matrix[max_row];
-        matrix[max_row] = temp;
-
-        if (std::abs(matrix[k][k]) < 1e-12) {
-            continue; 
+        if (std::abs(matrix[v][i]) < 1e-12) {
+            return -1; //матрица вырождена
         }
 
-        double element = matrix[k][k];
-        for (int j = k; j < cols; j++) {
-            matrix[k][j] /= element;
-        }
-
-        for (int i = k + 1; i < rows; i++) {
-            double factor = matrix[i][k];
-            for (int j = k; j < cols; j++) {
-                matrix[i][j] -= factor * matrix[k][j];
+        if (v != i){
+            for (int k = i; k < cols; k++){
+                double temp = matrix[i][k];
+                matrix[i][k] = matrix[v][k];
+                matrix[v][k] = temp;
             }
-            matrix[i][k] = 0;
+        }
+
+        for (int k = i + 1; k < rows; k++) {
+            double factor = matrix[k][i]/matrix[i][i];
+            for (int j = i; j < cols; j++) {
+                matrix[k][j] -= factor * matrix[i][j];
+            }
         }
     }
+    for (int i = cols; i < rows; i++){
+        if (std::abs(matrix[i][cols-1]) > 1e-12) {
+            return 0; //матрица несовместна
+        }
+    }
+
+    return 1; //матрица невырождена и совместна
 }
 
-void solve_and_print(double **matrix, int rows, int cols) {
+/*void solve_and_print(double **matrix, int rows, int cols) {
     int n = cols - 1;
     double *x = new double[n];
 
@@ -71,21 +79,23 @@ void solve_and_print(double **matrix, int rows, int cols) {
     }
 
     delete[] x; 
-}
+}*/
 
 void solve_and_write_roots(FILE* out, double **matrix, int rows, int cols) {
     int n = rows; 
     double *x = new double[n];
-
+    fprintf(out, "Roots:\n");
     for (int i = n - 1; i >= 0; i--) {
         x[i] = matrix[i][cols - 1];
         for (int j = i + 1; j < n; j++) {
             x[i] -= matrix[i][j] * x[j];
         }
+        x[i] /= matrix[i][i]; 
     }
 
     for (int i = 0; i < n; i++) {
         fprintf(out, "x%d = %.2lf", i + 1, x[i]);
+        printf("x%d = %.2lf\n", i + 1, x[i]);
         if (i < n - 1) fprintf(out, "  "); 
     }
     fprintf(out, "\n");
@@ -98,7 +108,7 @@ int main() {
     FILE* output_file = fopen("output.txt", "w");
 
     if (input_file == nullptr) {
-        std::cout << "Error!" << std::endl;
+        printf("Error! \n");
         return 1;
     }
     int M, N;
@@ -112,18 +122,21 @@ int main() {
         }
     }
 
-    print_matrix(coeffitients, M, N);
-    printf("----- \n");
-
-    triangularize(coeffitients, M, N);
+    
+    int status = triangularize(coeffitients, M, N);
     fprintf(output_file, "Triangular Matrix:\n");
     print_matrix_to_file(output_file, coeffitients, M, N);
 
-    print_matrix(coeffitients, M, N);
-    solve_and_print(coeffitients, M, N);
+    if (status == 1){
+        solve_and_write_roots(output_file, coeffitients, M, N);
+    }
+    else if (status == 0){
+        printf("Inconsistent system \n");
+        fprintf(output_file, "Inconsistent system \n");
+    }
+    else if (status == -1){
 
-    fprintf(output_file, "Roots:\n");
-    solve_and_write_roots(output_file, coeffitients, M, N);
+    }
     
     for (int i = 0; i < M; i++) delete[] coeffitients[i];
     delete[] coeffitients;
